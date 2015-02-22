2564c65b2b188766936b2e86a3dc18a8d01aad40
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-02-22 10:08:41 -0500

    v1.0

---
+++ text2speech.py
@@ -0,0 +1,15 @@
+from Tkinter import *
+import os
+
+root = Tk()
+root.title("Text2Speech")
+T = Text(root, height=1, width=30)
+T.pack()
+
+def speak():
+    os.system("say " + T.get("1.0",END))
+
+B = Button(root, text ="Speak", command = speak)
+B.pack()
+T.insert(END, "Text Here will be spoken!")
+mainloop()
